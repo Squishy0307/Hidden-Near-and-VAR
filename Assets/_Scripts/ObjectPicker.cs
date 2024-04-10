899db4f3df68e8d349b94df212976152f61fbeb4
Squishy0307 <38110101+Squishy0307@users.noreply.github.com>
2024-04-10 12:48:47 -0400

    Models and Dust particels

--- Assets/_Scripts/ObjectPicker.cs
+++ Assets/_Scripts/ObjectPicker.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.VFX;
 
 public class ObjectPicker : MonoBehaviour
 {
     private Camera cam;
     [SerializeField] LayerMask intractableLayer;
+    [SerializeField] VisualEffect dust;
 
     void Start()
     {
@@ -21,9 +23,17 @@ public class ObjectPicker : MonoBehaviour
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 500, intractableLayer))
+            RaycastHit hit2;
+
+            if (Physics.Raycast(ray, out hit, 500,intractableLayer))
             {
                 FoundAnObject(hit.transform.gameObject);
+            }
+
+            if(Physics.Raycast(ray, out hit2, 500))
+            {
+                dust.SendEvent("Fire");
+                dust.SetVector3("Pos", hit2.point);
             }
         }
     }
