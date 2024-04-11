98e71020a0541fc52aec0fefc996603b6c1bc7e4
Squishy0307 <38110101+Squishy0307@users.noreply.github.com>
2024-04-11 14:44:36 -0400

    Floor Texture

--- Assets/_Scripts/Wall.cs
+++ Assets/_Scripts/Wall.cs
@@ -3,6 +3,7 @@ using DG.Tweening;
 
 public class Wall : MonoBehaviour
 {
+    private MeshRenderer mesh;
     private Material mat;
     private float dittherAmount = 2;
 
@@ -13,7 +14,8 @@ public class Wall : MonoBehaviour
 
     void Start()
     {
-        mat = transform.GetComponent<MeshRenderer>().material;
+        mesh = transform.GetComponent<MeshRenderer>();
+        mat = mesh.material;
 
         if (StartDisapper)
         {
@@ -33,13 +35,23 @@ public class Wall : MonoBehaviour
     public void Apper()
     {
         DisapperTween.Kill();
+        enableMesh();
         AppearTween = DOTween.To(() => dittherAmount, x => dittherAmount = x, 2f, 0.35f);
     }
 
     public void Disapper()
     {
         AppearTween.Kill();
-        DisapperTween = DOTween.To(() => dittherAmount, x => dittherAmount = x, 0f, 0.35f);
+        DisapperTween = DOTween.To(() => dittherAmount, x => dittherAmount = x, 0f, 0.35f).OnComplete(disableMesh);
+    }
+
+    void enableMesh()
+    {
+        mesh.enabled = true;
+    }
+    void disableMesh()
+    {
+        mesh.enabled = false;
     }
 
 }
